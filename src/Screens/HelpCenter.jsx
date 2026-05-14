aaaa112e1ac38b9236e67541ea79f24a29c6e6a2
Dnyaneshwar-Baride464 <dnyaneshwarbaride@q2pay.in>
2026-05-14 10:30:47 +0530

    heplupdate

--- src/Screens/HelpCenter.jsx
+++ src/Screens/HelpCenter.jsx
@@ -11,58 +11,84 @@ import Margins from '../styles/margin'
 
 
 const HepCenter = () => {
-  const [userChat, setUserChat] = useState(false)
+  // const [userChat, setUserChat] = useState(false)
 
   // const [message, setMessage] = useState('')
-  // const [chat, setChat] = ([
-  //   {
-  //     id: 8,
-  //     type: bot,
-  //     questions: 'How can i help you?'
-  //   }
-  // ])
-  // setChat(chat)
-  const sendMessage = async () => {
-    try {
-      const Response = await axios.get('https://699c3665110b5b738cc20669.mockapi.io/api/todo/')
-      console.log('response', Response)
-      setUserChat(Response.data)
-    } catch (error) {
-      console.log(error)
+  const [chat, setChat] = ([
+    {
+      id: 8,
+      type: "bot",
+      questions: 'How can i help you?'
     }
-
+  ])
+  // setChat(chat)
+  const sendMessage = (question) => {
+    let Botreply = ""
+    if (question === 'order') {
+      Botreply = "Your order will be deliverd within 3 - to4 days"
+    } else if (question === 'payment') {
+      Botreply = " please check your internet connection and the account balace too"
+    } else if (question === 'refund') {
+      Botreply = " refund will be credited to your account within 5 days"
+    }
+    else if (question === 'delivery') {
+      Botreply = " sometime it is delayed because of hight volume."
+    }
+    else if (question === 'cancel') {
+      Botreply = " can cancel your order before it gets shipped, just click on cancel order."
+    }
+    setChat(prev => [
+      ...prev, {
+        type: 'user',
+        message: question,
+      },
+      {
+        type: 'bot',
+        message: Botreply
+      }
+    ])
   }
-  useEffect(() => {
-    sendMessage()
-  }, [])
+
+
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <View style={{ paddingLeft: Padding.m, borderBottomWidth: Borders.exsmall, borderColor: '#bc737f ' }}>
+      {
+        chat.map((item, index) => (
+          <View key={index} style={styles.chats}>
+            <Text>
+              {item.message}
+            </Text>
+          </View>
+        )
+
+        )
+      }
+      {/* <View style={{ paddingLeft: Padding.m, borderBottomWidth: Borders.exsmall, borderColor: '#bc737f ' }}>
         <Text style={styles.HelpCentTxt}> Help Center ??</Text>
-      </View>
+      </View> */}
       <View style={{ borderWidth: Borders.s, borderRadius: Radius.XXXXL, width: Sizes.SizeOEF, padding: Padding.ss, backgroundColor: '#ddf9f3' }}>
         <Text style={{ textAlign: 'center', fontWeight: 'bold' }}> How can I help you ?</Text>
       </View>
       <View style={{ flexDirection: 'row', gap: 6, }}>
-        <TouchableOpacity style={styles.issues} onPress={() => sendMessage(!userChat)}>
+        <TouchableOpacity style={styles.issues} onPress={() => sendMessage("order")}>
           <Text style={styles.txts}>order</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.issues}>
+        <TouchableOpacity style={styles.issues} onPress={() => sendMessage("payment")}>
           <Text style={styles.txts}>payment</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.issues}>
+        <TouchableOpacity style={styles.issues} onPress={() => sendMessage("refund")}>
           <Text style={styles.txts}>refund</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.issues}>
+        <TouchableOpacity style={styles.issues} onPress={() => sendMessage("delivery")}>
           <Text style={styles.txts}>delivery</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.issues}>
+        <TouchableOpacity style={styles.issues} onPress={() => sendMessage("cancel")}>
           <Text style={styles.txts}>cancel Order</Text>
         </TouchableOpacity>
       </View>
 
 
-      <View style={styles.userChats}>
+      {/* <View style={styles.userChats}>
         <Text style={styles.quesion}>user:  where is my order?</Text>
       </View>
 
@@ -108,7 +134,7 @@ const HepCenter = () => {
 
       <View style={styles.chats}>
         <Text style={styles.answer}>Bot  :  Thank you!! Never come back here😠</Text>
-      </View>
+      </View> */}
 
 
     </ScrollView>
